f836256ad8528a6b0624f5dd40b915b5e8300f6c
Kamil Kowalski <nightfox09876@gmail.com>
2024-11-03 20:43:37 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp/Osoba.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class Osoba
+    {
+        private int Id;
+        private string Imie;
+
+        public static int LiczbaInstancji = 0;
+        public Osoba()
+        {
+            Id = 0;
+            Imie = "";
+            LiczbaInstancji++;
+        }
+        public Osoba(int id, string imie)
+        {
+            Id = id;
+            Imie = imie;
+            LiczbaInstancji++;
+        }
+        public Osoba(Osoba osoba)
+        {
+            Id = osoba.Id;
+            Imie = osoba.Imie;
+            LiczbaInstancji++;
+        }
+        public void Przywitanie(string argument)
+        {
+            if (string.IsNullOrEmpty(Imie))
+            {
+                Console.WriteLine("Brak danych");
+            }
+            else
+            {
+                Console.WriteLine($"Cześć {argument}, mam na imię {Imie}");
+            }
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Liczba zarejestrowanych osób to {Osoba.LiczbaInstancji}");
+
+            Osoba osoba1 = new Osoba();
+
+            Console.Write("Podaj ID osoby: ");
+            int id = int.TryParse(Console.ReadLine(),out int ID) ? ID : 0;
+            Console.Write("Podaj imię osoby: ");
+            string? Imie = Console.ReadLine();
+            Imie = string.IsNullOrEmpty(Imie) ? "" : Imie;
+
+            Osoba osoba2 = new Osoba(id, Imie);
+
+            Osoba osoba3 = new Osoba(osoba2);
+
+            Console.WriteLine("Przywitanie dla obiektu osoba1:");
+            osoba1.Przywitanie("Jan");
+
+            Console.WriteLine("Przywitanie dla obiektu osoba2:");
+            osoba2.Przywitanie("Jan");
+
+            Console.WriteLine("Przywitanie dla obiektu osoba3:");
+            osoba3.Przywitanie("Jan");
+
+            Console.WriteLine($"Liczba zarejestrowanych osób to {Osoba.LiczbaInstancji}");
+        }
+    }
+}
